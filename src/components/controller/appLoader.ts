72071dc08687314485648695786f06a02b5a1842
RichardPickman <drezzerock@gmail.com>
2022-06-05 15:17:40 +0200

    feat: replace controller and appLoaders to ts extensions

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,9 +1,10 @@
 import Loader from './loader';
+import { LoaderInterface } from '../../interfaces';
 
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: process.env.NEWS_API, // получите свой ключ https://newsapi.org/
         });
     }
 }
