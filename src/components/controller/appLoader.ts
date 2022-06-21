b4adcd48ea97b74af9943e388ed7dbf643162670
RichardPickman <drezzerock@gmail.com>
2022-06-21 20:23:51 +0200

    refactor: minor refactor

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,11 +1,8 @@
 import Loader from './loader';
-import { LoaderInterface } from '../../interfaces';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: process.env.NEWS_API, // получите свой ключ https://newsapi.org/
-        });
+        super('https://newsapi.org/v2/', { apiKey: process.env.NEWS_API });
     }
 }
 
