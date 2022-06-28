0f428390ea6fb7cd98dd12d894608045f6f52a87
RichardPickman <drezzerock@gmail.com>
2022-06-28 17:07:48 +0200

    feat: add default image

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -15,7 +15,7 @@ class News implements NewsInteface {
             if (idx % 2) (newsClone.querySelector('.news__item') as HTMLElement).classList.add('alt');
 
             (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
+                item.urlToImage || '/assets/img/news_placeholder.jpg'
             })`;
             (newsClone.querySelector('.news__meta-author') as HTMLElement).textContent =
                 item.author || item.source.name;
